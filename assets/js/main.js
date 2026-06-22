79bdc809b5604ab7be002a3317ce2f2961a2d654
uptobe8 <ia.uptobe@gmail.com>
2026-06-22 16:37:46 +0200

    sss

--- assets/js/main.js
+++
@@ -1,3 +0,0 @@
-document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>{const t=document.querySelector('#nav-toggle'); if(t) t.checked=false;}));
-
-document.querySelectorAll('.ux-accordion-item > button').forEach(btn=>{btn.addEventListener('click',()=>{btn.parentElement.classList.toggle('is-open')})});
